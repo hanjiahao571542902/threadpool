db3ebe8844f61c1f88f853a62c2d7a92e7989bea
jiahao han <571542902@qq.com>
2024-08-23 09:17:38 +0800

    实现sort

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 using namespace std;
-void sort(int arr[],int size)
+void sort(int arr[], int size)
 {
-	
+    // asjkdhksanf
 }
-int main(){
-int arr[]={12,4,89,43,21,78};
-int size=sizeof(arr)/sizeof(arr[0]);
-sort(arr,size);
-return 0;
+int main()
+{
+    int arr[] = {12, 4, 89, 43, 21, 78};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    sort(arr, size);
+    return 0;
 }
